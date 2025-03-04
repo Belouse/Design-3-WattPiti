36dff9ad7a33af85bd1cca81255065af00dde6ad
THAUG6 <117377205+THAUG6@users.noreply.github.com>
2025-03-04 10:24:55 -0500

    Initial commit

--- Interface/UI.py
+++ Interface/UI.py
@@ -1,2 +1,46 @@
+from mytk import *
+import time
+from tkinter import ttk
+from tkinter import *
 
-print("Hello interface")
+
+# Création de la classe de l'interface graphique pour utilisateur
+class InterfaceWattpiti(App):
+    def __init__(self):
+        App.__init__(self)
+
+        self.window.widget.title("Puissance-mètre Wattpiti") # Nom de la fenêtre
+        self.window.widget.geometry("400x400")
+        self.progress = ttk.Progressbar(self.window.widget, length=200)
+        self.progress.pack(pady=20)
+        
+        self.start_button = Button(self.window.widget, text = "Start",  command = self.start_progress)
+        self.start_button.pack(pady=10)
+        self.stop_button = Button(self.window.widget, text = "Stop", command = self.stop_progress)
+        self.stop_button.pack(pady=10)
+        self.stop = False
+
+    def start_progress(self):
+        self.start_button.pack_forget()
+        self.progress.pack_forget()
+        self.stop_button.pack_forget()
+        self.progress['value'] = 0
+        self.window.widget.update_idletasks()
+        for i in range(101):
+            if self.stop:
+                break
+            time.sleep(0.05)
+            self.progress['value'] = i
+            self.window.widget.update_idletasks()
+        self.start_button.pack(pady=10)
+        self.progress.pack(pady=20)
+        self.stop_button.pack(pady=10)
+
+    def stop_progress(self):
+        self.stop = True
+
+
+# Lancement de l'interface graphique
+if __name__ == "__main__":
+    app = InterfaceWattpiti()
+    app.mainloop()
