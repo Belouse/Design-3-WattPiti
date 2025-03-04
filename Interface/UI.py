314cb2dff197fc3a8f401f6ff369f00ee8dc6248
THAUG6 <117377205+THAUG6@users.noreply.github.com>
2025-03-04 13:08:23 -0500

    Refactor UI layout and update button functionality in InterfaceWattpiti

--- Interface/UI.py
+++ Interface/UI.py
@@ -1,7 +1,4 @@
 from mytk import *
-import time
-from tkinter import ttk
-from tkinter import *
 
 
 # Création de la classe de l'interface graphique pour utilisateur
@@ -9,35 +6,15 @@ class InterfaceWattpiti(App):
     def __init__(self):
         App.__init__(self)
 
-        self.window.widget.title("Puissance-mètre Wattpiti") # Nom de la fenêtre
-        self.window.widget.geometry("400x400")
-        self.progress = ttk.Progressbar(self.window.widget, length=200)
-        self.progress.pack(pady=20)
-        
-        self.start_button = Button(self.window.widget, text = "Start",  command = self.start_progress)
-        self.start_button.pack(pady=10)
-        self.stop_button = Button(self.window.widget, text = "Stop", command = self.stop_progress)
-        self.stop_button.pack(pady=10)
-        self.stop = False
+        self.window.widget.title("Puissance-mètre Wattpiti")
+        self.window.widget.geometry("800x800")
+        self.start_button = Button("Commencer", user_event_callback=self.click_start)
+        self.start_button.grid_into(self.window, row = 0, column = 0, padx = 500, pady = 400, sticky = "ns")
 
-    def start_progress(self):
-        self.start_button.pack_forget()
-        self.progress.pack_forget()
-        self.stop_button.pack_forget()
-        self.progress['value'] = 0
-        self.window.widget.update_idletasks()
-        for i in range(101):
-            if self.stop:
-                break
-            time.sleep(0.05)
-            self.progress['value'] = i
-            self.window.widget.update_idletasks()
-        self.start_button.pack(pady=10)
-        self.progress.pack(pady=20)
-        self.stop_button.pack(pady=10)
 
-    def stop_progress(self):
-        self.stop = True
+    def click_start(self, event, button):
+        self.start_button.grid_into(self.window, row = 800, column = 800, padx = 0, pady = 0, sticky = "ns")
+            
 
 
 # Lancement de l'interface graphique
