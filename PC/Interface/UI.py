245f97b69ae2e7d694031cb239b3a23df8fd4959
THAUG6 <117377205+THAUG6@users.noreply.github.com>
2025-03-09 09:23:06 -0400

    changes UI 1

--- PC/Interface/UI.py
+++ PC/Interface/UI.py
@@ -10,7 +10,13 @@ class InterfaceWattpiti(tk.Tk):
         #création de l'interface
         self.title("Puissance-mètre Wattpiti")
         self.geometry("1800x1800")
-        self.configure(background="grey")
+        self.configure(background="white")
+
+
+        mystyle = ttk.Style()
+        mystyle.theme_use('alt')   # choose other theme
+        mystyle.configure('MyStyle.TLabelframe', background = "white", borderwidth=10, relief='solid', labelmargins=20)
+        mystyle.configure('MyStyle.TLabelframe.Label', font=('Inter', 11, 'bold'))
 
 
         #Création de la grille pour le logo
@@ -31,60 +37,53 @@ class InterfaceWattpiti(tk.Tk):
         self.logo = ImageTk.PhotoImage(self.logo)
         self.labelLogo.configure(image=self.logo)
 
-        #self.start_button = ttk.Button(self, text="Commencer", command=self.click_start)
-        #self.start_button.grid(row=5, column=5, padx=5, pady=5, sticky="nsew")
+        #Création d'un cadre pour les paramètres de configuration
+        self.configFrame = ttk.Frame(self, width=200, height=350, borderwidth=3, style='MyStyle.TLabelframe')
+        self.configFrame.grid(row=1, column=0, pady=5, padx=5, sticky="nsew")
+        self.configFrame.grid_propagate(False)
 
+        # Création d'un titre pour les paramètres de configuration
+        self.labelConfig = ttk.Label(self.configFrame, text="Configuration", style = 'MyStyle.TLabelframe.Label')
+        self.labelConfig.grid(row=0, column=1, padx=5, pady=5, sticky = "nsew")
+
+        #Création d'un bouton pour démarrer la simulation
+        self.startButton = ttk.Button(self, text="Commencer", command=self.click_start)
+        self.startButton.place(x = 65, y= 280)
+
+        #Création d'un bouton pour arrêter la simulation
+        self.stopButton = ttk.Button(self, text="Arrêt", command=self.click_stop)
+        self.stopButton.place(x=65, y=320)
+
+        #Création d'un bouton pour réinitialiser la simulation
+        self.resetButton = ttk.Button(self, text='Reset', command=self.click_reset)
+        self.resetButton.place(x=65, y=360)
+
+        #Création d'un label pour la fréquence d'échantillonnage
+        self.labelFreq = ttk.Label(self, text="Fréquence d'échantillonnage (Hz)")
+        self.labelFreq.place(x=64, y = 400)
+
+        # Création d'un cadre pour les options d'enregistrement
+        self.optionsFrame = ttk.Frame(self, width=200, height=350, borderwidth=3, style='MyStyle.TLabelframe')
+        self.optionsFrame.grid(row=2, column=0, pady=5, padx=5, sticky="nsew")
+        self.optionsFrame.grid_propagate(False)
+
+        # Création d'un titre pour les options d'enregistrement
+        self.labelOptions = ttk.Label(self.optionsFrame, text="Options d'enregistrement", style='MyStyle.TLabelframe.Label')
+        self.labelOptions.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+
+
+    #Fonction du bouton pour démarrer la simulation
     def click_start(self):
         pass
 
-if __name__ == "__main__":
-    app = InterfaceWattpiti()
-    app.mainloop()
-
-
-
-
-
-
-
-"""
-from mytk import *
-import os
-# Création de la classe de l'interface graphique pour utilisateur
-class InterfaceWattpiti(App):
-    def __init__(self):
-        App.__init__(self)
-
-        self.window.widget.title("Puissance-mètre Wattpiti")
-        self.window.widget.geometry("1800x1800")
-
-        self.window.row_resize_weight(0,0)
-        self.window.row_resize_weight(1,0)
-        self.window.row_resize_weight(2,1)
-        self.window.column_resize_weight(0,1)
-        self.window.column_resize_weight(1,1)
-
-
-        self.view = View(width=1000, height=1000)
-        self.view.grid_into(self.window, row =0, column = 0, pady=5, padx=5, sticky="nsew")
-        self.view.grid_propagate(True)
-        dir = os.path.dirname(__file__)
-        self.logo = Image(os.path.join(dir,"logoWattpiti.jpg"))
-        self.logo.grid_into(self.view, row = 1, column = 0, padx = 5, pady = 5, sticky ="")
-
-
-
-        #self.start_button = Button("commencer",  user_event_callback= self.click_start)
-        #self.start_button.grid_into(self.window, row = 0, column = 0, padx = 100, pady = 100, sticky = "nsew")
-
-
-    def click_start(self, button, event):
+    #Fonction du bouton pour arrêter la simulation
+    def click_stop(self):
         pass
 
-
-# Lancement de l'interface graphique
+    #Fontion du bouton pour réinitialiser la simulation
+    def click_reset(self):
+        pass
 
 if __name__ == "__main__":
     app = InterfaceWattpiti()
     app.mainloop()
-"""
